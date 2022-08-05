57aafc1d6b46f8798ba6c1593364aab64f8601c9
luucasgb <luucasguimaraesb@gmail.com>
2022-08-05 01:12:35 -0300

    Added print for each file being currently copied

--- dlc_mover.py
+++ dlc_mover.py
@@ -1,22 +1,27 @@
-from genericpath import isfile
 import os
 import shutil
 
+# Folder from where DLC should be copied
 root_folder = "E:\\Torrents\\Rocksmith DLC"
-target_folder = "D:\\Lucas\\Downloads\\Teste"
-count = [0]
 
-def traverse_folder_structure(path, x):
+# Folder where Rocksmith DLC is stored
+target_folder = "D:\\Lucas\\Downloads\\Teste"
+
+# Array with a single item, used to keep a count of files moved by script
+moved_files = [0]
+
+def traverse_folder_structure(path, count):
 	path_content = os.listdir(path)
 	if len(path_content) != 0:
 		for item in path_content:
 			current_item = os.path.join(path, item)
 			if not os.path.isfile(current_item):
-				traverse_folder_structure(os.path.join(path, current_item), x)
+				traverse_folder_structure(os.path.join(path, current_item), count)
 				continue
 			if current_item.endswith("_p.psarc"):
-				x[0] += 1
+				count[0] += 1
+				print("Current file: {}".format(item))
 				shutil.copy2(current_item, target_folder)
 
-traverse_folder_structure(root_folder, count)
-print("{} files were moved.}".format(count))
+traverse_folder_structure(root_folder, moved_files)
+print("{} files were moved.".format(moved_files[0]))
